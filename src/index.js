800ec63445d8f09157f3ffadf85358732fbc0594
Dan Morgan <hello@danny.codes>
2020-04-04 16:58:52 -0400

    add personal site test config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const path = require('path')
+const fs = require('fs').promises
 const { parse } = require('./parse-config')
+const { generate } = require('./generate')
 
-async function main({ input = path.join(__dirname, 'typebeast.yml') } = {}) {
+async function main({
+  input = path.join(__dirname, 'typebeast.yml'),
+  output = path.join(__dirname, 'typebeast.css'),
+} = {}) {
   const config = await parse(input)
   // TODO: assert validation of config file before proceeding
-  console.log(config)
+  const generated = await generate(config)
+  await fs.writeFile(output, generated)
 }
 
 main()
