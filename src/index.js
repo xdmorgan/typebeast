49741eded69e6dc022dd4a93d5575d25070c329a
Dan Morgan <hello@danny.codes>
2020-04-11 10:08:58 -0400

    Dynamically write styles separate from mixins

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const fs = require('fs-extra')
 const { parse } = require('./parse-config')
 const { transform } = require('./transform-config')
 const { write: writeMixins } = require('./write-mixins')
+const { write: writeStyles } = require('./write-styles')
 
 async function main({
   input = path.join(__dirname, 'typebeast.yml'),
@@ -13,8 +14,12 @@ async function main({
   // TODO: assert validation of config file before attempting merge
   // TODO: merge custom config onto defaults for things like prefixes
   const transformed = await transform(config)
+  // write mixin definitions
   const mixins = writeMixins({ data: transformed })
   await fs.writeFile(path.join(outdir, 'generated-mixins.scss'), mixins)
+  // write classes that refer to the mixins
+  const styles = writeStyles({ data: transformed, config })
+  await fs.writeFile(path.join(outdir, 'generated-styles.scss'), styles)
 }
 
 main()
