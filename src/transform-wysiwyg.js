7dbcb780d159920c085b58868957f493da353985
Dan Morgan <hello@danny.codes>
2020-06-03 20:55:05 -0400

    Transform config for wysiwyg spacing

---
+++ src/transform-wysiwyg.js
@@ -0,0 +1,88 @@
+function getScopeFromConfig(config) {
+  return '.' + config.wysiwyg.scope
+}
+
+// TODO: copied from transform-spacing, condense at some point
+function getSpacingPropertyByKey(props, key) {
+  if (props[key] === undefined) return {}
+  if (key === 'block') {
+    return {
+      'margin-block-start': props[key],
+      'margin-block-end': props[key],
+    }
+  }
+  if (key === 'inline') {
+    return {
+      'margin-inline-start': props[key],
+      'margin-inline-end': props[key],
+    }
+  }
+  return { [`margin-${key}`]: props[key] }
+}
+
+function getSpacingProperties(properties) {
+  return {
+    ...getSpacingPropertyByKey(properties, 'block'),
+    ...getSpacingPropertyByKey(properties, 'block-start'),
+    ...getSpacingPropertyByKey(properties, 'block-end'),
+    ...getSpacingPropertyByKey(properties, 'inline'),
+    ...getSpacingPropertyByKey(properties, 'inline-start'),
+    ...getSpacingPropertyByKey(properties, 'inline-end'),
+  }
+}
+
+function getBreakpointWritersFromConfig(config) {
+  return Object.entries(config.breakpoints).reduce(
+    (all, [name, size]) => ({
+      ...all,
+      [name]: body => `@include typebeast-break-min(${size}){ ${body} }`,
+    }),
+    {
+      default: body => body,
+    }
+  )
+}
+
+function getGroupsFromConfig(config) {
+  return Object.entries(config.wysiwyg.spacing).reduce(
+    (allGroups, [groupName, groupData]) => ({
+      ...allGroups,
+      [groupName]: {
+        selectors: groupData.include.join(', '),
+        breakpoints: Object.entries(groupData.breakpoints).reduce(
+          (allBreaks, [breakName, breakData]) => ({
+            ...allBreaks,
+            [breakName]: getSpacingProperties(breakData),
+          }),
+          {}
+        ),
+      },
+    }),
+    {}
+  )
+}
+
+function transform(config) {
+  return {
+    // '.classname'
+    scope: getScopeFromConfig(config),
+    // breakpoints: {
+    //   default: body => body,
+    //   large: body => `@include typebeast-break-min(768px){ ${body} }`,
+    // },
+    breakpoints: getBreakpointWritersFromConfig(config),
+    // groups: {
+    //   'vertical-rhythm': {
+    //     default: {
+    //       selectors: `h1, h2, h3, h4, h5, h6, p, ul, ol, dl, blockquote, figure, pre`,
+    //       properties: {
+    //         'margin-block-end': '1rem',
+    //       },
+    //     },
+    //   },
+    // }
+    groups: getGroupsFromConfig(config),
+  }
+}
+
+module.exports = { transform }
